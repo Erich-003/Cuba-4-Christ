a2dcff1ba68df331b58b6b82526994455ed2e9fc
Kumagaro <kumsanzer2@gmail.com>
2023-10-21 18:49:55 -0400

    Carrusel

---
+++ Carrusel/carrusel.js
@@ -0,0 +1,64 @@
+let imagenes = [
+    {
+        "url": "./Carrusel/img/30.jpg",
+        "nombre": "Proyecto 01",
+       
+    },
+    {
+        "url": "./Carrusel/img/6.jpg",
+        "nombre": "Proyecto 02",
+      
+    },
+    {
+        "url": "./Carrusel/img/7.jpg",
+        "nombre": "Proyecto 03",
+       
+    },
+]
+
+
+let atras = document.getElementById('atras');
+let adelante = document.getElementById('adelante');
+let imagen = document.getElementById('img');
+let puntos = document.getElementById('puntos');
+let texto = document.getElementById('texto')
+let actual = 0
+posicionCarrusel()
+
+atras.addEventListener('click', function(){
+    actual -=1
+
+    if (actual == -1){
+        actual = imagenes.length - 1
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+adelante.addEventListener('click', function(){
+    actual +=1
+
+    if (actual == imagenes.length){
+        actual = 0
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+
+    posicionCarrusel()
+})  
+
+function posicionCarrusel() {
+    puntos.innerHTML = ""
+    for (var i = 0; i <imagenes.length; i++){
+        if(i == actual){
+            puntos.innerHTML += '<p class="bold">.<p>'
+        }
+        else{
+            puntos.innerHTML += '<p>.<p>'
+        }
+    } 
+}
